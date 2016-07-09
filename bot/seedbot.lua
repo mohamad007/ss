aaea9764f4058a5e3c2619e36c1e2568e47af73f
mohamad007 <mohamad137977@gmail.com>
2016-07-09 16:08:12 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -214,7 +214,6 @@ function create_config( )
     "inpm",
     "banhammer",
     "stats",
-    "anti_spam",
     "owners",
     "arabic_lock",
     "set",
@@ -226,7 +225,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {208094932,126355654},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
